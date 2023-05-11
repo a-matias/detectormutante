4759fac4c3be32633fd0d355f8aa69ecdefec088
Matias Andres <matiasandres@cnrt.ar>
2023-05-11 10:52:02 -0300

    modificacion en ruta

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MutanteController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('mutante');
 });
+
+Route::post('/mutante', [$MutanteController, 'detectar'])->name('mutacion');
